058acf01309f6912424a8e8d42ff63fb17f0cb44
Duke <114975455+RaveZam@users.noreply.github.com>
2024-10-22 15:20:00 +0800

    Vue Practice

---
+++ vue-practice/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+    <button @click="logMessage">Click</button> <br />
+    <button @click="logHelloMessage">Click</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  methods: {
+    logMessage(): void {
+      console.log("Button was pressed!");
+    },
+
+    logHelloMessage(): void {
+      console.log("Hello World");
+    },
+  },
+});
+</script>
