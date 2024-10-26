8cf5490b44528c61fdea2acdb0cd326f725ce323
Duke <114975455+RaveZam@users.noreply.github.com>
2024-10-26 23:16:15 +0800

    Test

--- vue-practice/src/App.vue
+++ vue-practice/src/App.vue
@@ -1,16 +1,22 @@
 <template>
+  <Header />
   <div>
     <h1>Hello</h1>
     <h1>{{ counter }}</h1>
-    <button @click="logMessage">Click</button> <br />
+    <button @click="decrementValue">Click</button> <br />
     <button @click="logHelloMessage">Click</button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Header from "./components/Header.vue";
 
 export default defineComponent({
+  components: {
+    Header,
+  },
+
   data() {
     return {
       counter: 0,
@@ -18,8 +24,12 @@ export default defineComponent({
   },
 
   methods: {
-    logMessage(): void {
-      console.log("Button was pressed!");
+    decrementValue(): void {
+      if (this.counter > 0) {
+        this.counter--;
+      } else {
+        null;
+      }
     },
 
     logHelloMessage(): void {
---
+++ vue-practice/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-gray-500 w-screen h-[10vh]">
+    <h1 class="font-lg">This is the header</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Header",
+  data() {
+    return {
+      firstNumber: 0,
+      secondNumber: 0,
+    };
+  },
+});
+</script>
